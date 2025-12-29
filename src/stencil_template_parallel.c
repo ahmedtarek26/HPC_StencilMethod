fd0eb18d58c8a149cd9c9c1498a220888431bbec
ahmed tarek elsayed <77227944+ahmedtarek26@users.noreply.github.com>
2025-12-29 01:33:44 +0100

    Update stencil_template_parallel.c file

--- src/stencil_template_parallel.c
+++ src/stencil_template_parallel.c
@@ -94,18 +94,21 @@ int main(int argc, char **argv)
       const int full_sizex = sizex + 2;
       const int full_sizey = sizey + 2;
 
+      // Pointers for N/S (contiguous)
       buffers[SEND][NORTH] = current_plane + full_sizex;
       buffers[SEND][SOUTH] = current_plane + sizey * full_sizex;
       buffers[RECV][NORTH] = current_plane;
       buffers[RECV][SOUTH] = current_plane + (sizey + 1) * full_sizex;
 
+      // Allocate E/W buffers only once
       if (buffers[SEND][EAST] == NULL) {
-        buffers[SEND][EAST] = (double*)malloc(sizey * sizeof(double));
-        buffers[RECV][EAST] = (double*)malloc(sizey * sizeof(double));
-        buffers[SEND][WEST] = (double*)malloc(sizey * sizeof(double));
-        buffers[RECV][WEST] = (double*)malloc(sizey * sizeof(double));
+        buffers[SEND][EAST] = malloc(sizey * sizeof(double));
+        buffers[RECV][EAST] = malloc(sizey * sizeof(double));
+        buffers[SEND][WEST] = malloc(sizey * sizeof(double));
+        buffers[RECV][WEST] = malloc(sizey * sizeof(double));
       }
 
+      // Fill E/W send buffers
       #pragma omp parallel for
       for (int j = 1; j <= sizey; j++) {
         buffers[SEND][WEST][j-1] = current_plane[j * full_sizex + 1];
@@ -136,12 +139,14 @@ int main(int argc, char **argv)
 
       total_comm_time += MPI_Wtime() - section_start_time;
 
+      // Copy received E/W halos
       #pragma omp parallel for
       for (int j = 1; j <= sizey; j++) {
         current_plane[j * full_sizex] = buffers[RECV][WEST][j-1];
         current_plane[j * full_sizex + sizex + 1] = buffers[RECV][EAST][j-1];
       }
 
+      // Non-periodic boundary: zero halo (infinite sink)
       if (!periodic) {
         if (neighbours[NORTH] == MPI_PROC_NULL) memset(current_plane, 0, full_sizex * sizeof(double));
         if (neighbours[SOUTH] == MPI_PROC_NULL) memset(current_plane + (sizey + 1) * full_sizex, 0, full_sizex * sizeof(double));
@@ -185,11 +190,9 @@ int main(int argc, char **argv)
   return 0;
 }
 
-/* --------------------------------------------------------------
-   Required functions (were missing — now implemented)
-   -------------------------------------------------------------- */
+/* Required supporting functions - must be in .c file */
 
-int memory_release ( plane_t *planes, buffers_t *buffers )
+int memory_release(plane_t *planes, buffers_t *buffers)
 {
   if (planes) {
     if (planes[OLD].data) free(planes[OLD].data);
@@ -206,33 +209,30 @@ int memory_release ( plane_t *planes, buffers_t *buffers )
   return 0;
 }
 
-int memory_allocate ( const uint neighbours[4],
-                      const vec2_t N,
-                      buffers_t *buffers_ptr,
-                      plane_t *planes_ptr )
+int memory_allocate(const uint neighbours[4], const vec2_t N, buffers_t *buffers_ptr, plane_t *planes_ptr)
 {
   if (!planes_ptr || !buffers_ptr) return 1;
 
   unsigned int frame_size = (planes_ptr[OLD].size[_x_] + 2) * (planes_ptr[OLD].size[_y_] + 2) * sizeof(double);
 
-  planes_ptr[OLD].data = (double*)malloc(frame_size);
+  planes_ptr[OLD].data = malloc(frame_size);
   if (!planes_ptr[OLD].data) return 1;
   memset(planes_ptr[OLD].data, 0, frame_size);
 
-  planes_ptr[NEW].data = (double*)malloc(frame_size);
+  planes_ptr[NEW].data = malloc(frame_size);
   if (!planes_ptr[NEW].data) return 1;
   memset(planes_ptr[NEW].data, 0, frame_size);
 
   uint sizey = planes_ptr[OLD].size[_y_];
-  (*buffers_ptr)[SEND][EAST] = (double*)malloc(sizey * sizeof(double));
-  (*buffers_ptr)[RECV][EAST] = (double*)malloc(sizey * sizeof(double));
-  (*buffers_ptr)[SEND][WEST] = (double*)malloc(sizey * sizeof(double));
-  (*buffers_ptr)[RECV][WEST] = (double*)malloc(sizey * sizeof(double));
+  (*buffers_ptr)[SEND][EAST] = malloc(sizey * sizeof(double));
+  (*buffers_ptr)[RECV][EAST] = malloc(sizey * sizeof(double));
+  (*buffers_ptr)[SEND][WEST] = malloc(sizey * sizeof(double));
+  (*buffers_ptr)[RECV][WEST] = malloc(sizey * sizeof(double));
 
   return 0;
 }
 
-int output_energy_stat ( int step, plane_t *plane, double budget, int Me, MPI_Comm *Comm )
+int output_energy_stat(int step, plane_t *plane, double budget, int Me, MPI_Comm *Comm)
 {
   double system_energy = 0.0;
   double tot_system_energy = 0.0;
@@ -246,187 +246,6 @@ int output_energy_stat ( int step, plane_t *plane, double budget, int Me, MPI_Co
     printf("total injected energy is %g, system energy is %g (avg %g per point)\n",
            budget, tot_system_energy, tot_system_energy / (plane->size[_x_] * plane->size[_y_]));
   }
-
-  return 0;
-}
-
-int initialize ( MPI_Comm *Comm,
-                 int       Me,
-                 int       Ntasks,
-                 int       argc,
-                 char    **argv,
-                 vec2_t   *S,
-                 vec2_t   *N,                 
-                 int      *periodic,
-                 int      *output_energy_stat_perstep,
-                 uint     *neighbours,
-                 int      *Niterations,
-                 int      *Nsources,
-                 int      *Nsources_local,
-                 vec2_t  **Sources_local,
-                 double   *energy_per_source,
-                 plane_t  *planes,
-                 buffers_t *buffers )
-{
-  int opt;
-  double freq = 0.0;
-  int verbose = 0;
-  vec2_t Grid;
-
-  (*S)[_x_] = 1000;
-  (*S)[_y_] = 1000;
-  *Nsources = 1;
-  *energy_per_source = 1.0;
-  *Niterations = 100;
-  *periodic = 0;
-  *output_energy_stat_perstep = 0;
-
-  while ((opt = getopt(argc, argv, "x:y:e:E:f:n:p:o:v:h")) != -1) {
-    switch (opt) {
-      case 'x': (*S)[_x_] = atoi(optarg); break;
-      case 'y': (*S)[_y_] = atoi(optarg); break;
-      case 'e': *Nsources = atoi(optarg); break;
-      case 'E': *energy_per_source = atof(optarg); break;
-      case 'f': freq = atof(optarg); break;
-      case 'n': *Niterations = atoi(optarg); break;
-      case 'p': *periodic = atoi(optarg); break;
-      case 'o': *output_energy_stat_perstep = atoi(optarg); break;
-      case 'v': verbose = atoi(optarg); break;
-      case 'h':
-        if (Me == 0) {
-          printf("Usage: %s [options]\n", argv[0]);
-          // print help...
-        }
-        MPI_Finalize();
-        exit(0);
-      default:
-        if (Me == 0) printf("Invalid option\n");
-        MPI_Finalize();
-        exit(1);
-    }
-  }
-
-  // injection frequency logic (your original)
-  int injection_frequency = (freq == 0.0) ? 1 : (int)(freq * *Niterations);
-
-  // domain decomposition (your original code)
-  int Nfactors;
-  uint *factors;
-  simple_factorization(Ntasks, &Nfactors, &factors);
-
-  Grid[_x_] = 1;
-  Grid[_y_] = Ntasks;
-  int sqrt_n = (int)sqrt(Ntasks);
-  for (int i = sqrt_n; i > 0; i--) {
-    if (Ntasks % i == 0) {
-      Grid[_x_] = i;
-      Grid[_y_] = Ntasks / i;
-      break;
-    }
-  }
-  free(factors);
-
-  (*N)[_x_] = Grid[_x_];
-  (*N)[_y_] = Grid[_y_];
-
-  int X = Me % Grid[_x_];
-  int Y = Me / Grid[_x_];
-
-  neighbours[NORTH] = (Y > 0) ? Me - Grid[_x_] : (*periodic ? Me + (Grid[_y_] - 1) * Grid[_x_] : MPI_PROC_NULL);
-  neighbours[SOUTH] = (Y < Grid[_y_] - 1) ? Me + Grid[_x_] : (*periodic ? Me - (Grid[_y_] - 1) * Grid[_x_] : MPI_PROC_NULL);
-  neighbours[WEST]  = (X > 0) ? Me - 1 : (*periodic ? Me + (Grid[_x_] - 1) : MPI_PROC_NULL);
-  neighbours[EAST]  = (X < Grid[_x_] - 1) ? Me + 1 : (*periodic ? Me - (Grid[_x_] - 1) : MPI_PROC_NULL);
-
-  vec2_t mysize;
-  uint s = (*S)[_x_] / Grid[_x_];
-  uint r = (*S)[_x_] % Grid[_x_];
-  mysize[_x_] = s + (X < r ? 1 : 0);
-  s = (*S)[_y_] / Grid[_y_];
-  r = (*S)[_y_] % Grid[_y_];
-  mysize[_y_] = s + (Y < r ? 1 : 0);
-
-  planes[OLD].size[_x_] = mysize[_x_];
-  planes[OLD].size[_y_] = mysize[_y_];
-  planes[NEW].size[_x_] = mysize[_x_];
-  planes[NEW].size[_y_] = mysize[_y_];
-
-  ret = memory_allocate(neighbours, *N, buffers, planes);
-  if (ret) return ret;
-
-  ret = initialize_sources(Me, Ntasks, Comm, mysize, *Nsources, Nsources_local, Sources_local);
-  if (ret) return ret;
-
-  return 0;
-}
-
-uint simple_factorization( uint A, int *Nfactors, uint **factors )
-{
-  int N = 0;
-  int f = 2;
-  uint _A_ = A;
-
-  while ( f * f <= _A_ ) {
-    while( _A_ % f == 0 ) {
-      N++;
-      _A_ /= f;
-    }
-    f++;
-  }
-  if (_A_ > 1) N++;
-
-  *Nfactors = N;
-  uint *_factors_ = (uint*)malloc( N * sizeof(uint) );
-
-  N = 0;
-  f = 2;
-  _A_ = A;
-
-  while ( f * f <= _A_ ) {
-    while( _A_ % f == 0 ) {
-      _factors_[N++] = f;
-      _A_ /= f;
-    }
-    f++;
-  }
-  if (_A_ > 1) _factors_[N++] = _A_;
-
-  *factors = _factors_;
-  return 0;
-}
-
-int initialize_sources( int Me,
-                        int Ntasks,
-                        MPI_Comm *Comm,
-                        vec2_t mysize,
-                        int Nsources,
-                        int *Nsources_local,
-                        vec2_t **Sources )
-{
-  srand48(time(NULL) ^ Me);
-  int *tasks_with_sources = (int*)malloc( Nsources * sizeof(int) );
-  
-  if ( Me == 0 ) {
-    for ( int i = 0; i < Nsources; i++ )
-      tasks_with_sources[i] = (int)lrand48() % Ntasks;
-  }
-  
-  MPI_Bcast( tasks_with_sources, Nsources, MPI_INT, 0, *Comm );
-
-  int nlocal = 0;
-  for ( int i = 0; i < Nsources; i++ )
-    nlocal += (tasks_with_sources[i] == Me);
-  *Nsources_local = nlocal;
-  
-  if ( nlocal > 0 ) {
-    vec2_t * helper = (vec2_t*)malloc( nlocal * sizeof(vec2_t) );      
-    for ( int s = 0; s < nlocal; s++ ) {
-      helper[s][_x_] = 1 + lrand48() % mysize[_x_];
-      helper[s][_y_] = 1 + lrand48() % mysize[_y_];
-    }
-    *Sources = helper;
-  }
-  
-  free( tasks_with_sources );
 
   return 0;
 }
